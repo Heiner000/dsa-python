026380e6e4a59c83455acfe4b77212e4beec3ad9
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-06-24 22:39:41 -0600

    develop data structure understanding with 3 things

---
+++ notes.py
@@ -0,0 +1,9 @@
+'''
+Referencing Data Structure and Algorithmic Thinking with Python by Karumanchi
+
+To develop a good understanding of a datastructure requires three things:
+1. Learn how the info is stored in the computer's memory,
+2. Become familiar with algorthims and how they manipulate the info in the data strucutres,
+3. Understand the performance characterstics of different data structures
+
+'''
